548af1c27c2f3a7f62073719ae7998dce34fe86d
cyChen <2273202874@qq.com>
2024-03-20 16:41:22 +0800

    demo更新
    Signed-off-by: cyChen <2273202874@qq.com>

--- FakeX/main.js
+++ FakeX/main.js
@@ -1,39 +1,26 @@
 import App from './App'
-import store from './store'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import './uni.promisify.adaptor'
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
+
 Vue.config.productionTip = false
-Vue.prototype.$store = store
-Vue.prototype.$adpid = "1111111111"
-Vue.prototype.$backgroundAudioData = {
-	playing: false,
-	playTime: 0,
-	formatedPlayTime: '00:00:00'
-}
 App.mpType = 'app'
 const app = new Vue({
-	store,
-	...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import {
-	createSSRApp
-} from 'vue'
+import { createSSRApp } from 'vue'
 export function createApp() {
-	const app = createSSRApp(App)
-	app.use(store)
-	app.config.globalProperties.$adpid = "1111111111"
-	app.config.globalProperties.$backgroundAudioData = {
-		playing: false,
-		playTime: 0,
-		formatedPlayTime: '00:00:00'
-	}
-	return {
-		app
-	}
+  const app = createSSRApp(App)
+  return {
+    app
+  }
 }
 // #endif
